f499fe47cf3c2309e4cf1804e55e5ee97a7c5300
LuigiBlood <luigiblood@gmail.com>
2021-05-25 21:49:56 +0200

    Add basic LC_LZ2 decompression

---
+++ TransComp/Decomp.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransComp
+{
+    public static class Decomp
+    {
+        public static class ISPK
+        {
+            public static int LC_LZ2(byte[] input, int addr, out byte[] output)
+            {
+                List<byte> buf = new List<byte>();
+                int cmd = -1;
+                int len = -1;
+                int i;
+                for (i = addr; i < input.Length; i++)
+                {
+                    switch (cmd)
+                    {
+                        case 0x00:  //Copy
+                            buf.Add(input[i]);
+                            len--;
+                            break;
+                        case 0x01:  //Byte Fill
+                            for (int j = 0; j < len; j++)
+                                buf.Add(input[i]);
+                            len = 0;
+                            break;
+                        case 0x02:  //Word Fill
+                            for (int j = 0; j < len; j++)
+                                buf.Add(input[i + (j & 1)]);
+                            i++;
+                            len = 0;
+                            break;
+                        case 0x03:  //Increment Fill
+                            int inc = input[i];
+                            for (int j = 0; j < len; j++)
+                            {
+                                buf.Add((byte)(inc & 0xFF));
+                                inc++;
+                            }
+                            len = 0;
+                            break;
+                        case 0x04:  //Repeat
+                            int rep = (input[i] * 0x100) + input[i + 1];
+                            if (rep >= buf.Count)
+                            {
+                                Console.WriteLine("Error: 0x" + i.ToString("X") + " repeat cmd has wrong address");
+                                output = new byte[0];
+                                return -1;
+                            }
+                            for (int j = 0; j < len; j++)
+                            {
+                                buf.Add(buf[rep + j]);
+                            }
+                            i++;
+                            len = 0;
+                            break;
+                        default:
+                            if (input[i] == 0xFF)
+                            {
+                                cmd = 0xFF;
+                            }
+                            else if (input[i] >= 0xE0)
+                            {
+                                cmd = (input[i] >> 2) & 7;
+                                len = (input[i] & 3) * 0x100; i++;
+                                len += input[i];
+                                //Console.WriteLine("Test: 0x" + (i - 2).ToString("X") + " - CMD 0x" + cmd.ToString("X") + " / LEN 0x" + len.ToString("X"));
+                            }
+                            else
+                            {
+                                cmd = (input[i] >> 5) & 7;
+                                len = input[i] & 0x1F;
+                                //Console.WriteLine("Test: 0x" + (i - 1).ToString("X") + " - CMD 0x" + cmd.ToString("X") + " / LEN 0x" + len.ToString("X"));
+                            }
+                            len++;
+                            break;
+                    }
+
+                    //Stop Command
+                    if (len == 0)
+                        cmd = -1;
+
+                    //Stop Decomp
+                    if (cmd == 0xFF)
+                        break;
+                }
+                int csize = i - addr;
+                int dsize = buf.Count;
+                output = buf.ToArray();
+                return 0;
+            }
+        }
+    }
+}
--- TransComp/Program.cs
+++ TransComp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,91 @@ namespace TransComp
     {
         static void Main(string[] args)
         {
+            if (args.Length > 0)
+            {
+                int arg_verbose = SearchArg(args, "-v");
+                int arg_comp = SearchArg(args, "-c");
+                int arg_decomp = SearchArg(args, "-d");
+                int arg_input = SearchArg(args, "-i");
+                int arg_output = SearchArg(args, "-o");
+                int arg_addr = SearchArg(args, "-a");
+
+                if (arg_comp != -1 && arg_decomp != -1)
+                {
+                    ShowVersion();
+                    Console.WriteLine("Error: Both Compress and Decompress?");
+                }
+                else if (arg_comp == -1 && arg_decomp == -1)
+                {
+                    ShowVersion();
+                    Console.WriteLine("Error: No Compress and Decompress");
+                }
+
+                if (arg_input == -1)
+                {
+                    ShowVersion();
+                    Console.WriteLine("Error: No input file");
+                }
+
+                if (arg_output == -1)
+                {
+                    ShowVersion();
+                    Console.WriteLine("Error: No input file");
+                }
+
+                string compformat = args[Math.Max(arg_comp, arg_decomp) + 1];
+                string inputfile = args[arg_input + 1];
+                string outputfile = args[arg_output + 1];
+                bool isComp = (arg_comp != -1);
+
+                if (isComp)
+                {
+                    //Compression
+                }
+                else
+                {
+                    //Decompression
+                    byte[] outbuf = new byte[0];
+
+                    if (compformat == "lz2")
+                    {
+                        Decomp.ISPK.LC_LZ2(File.ReadAllBytes(inputfile), 0, out outbuf);
+                    }
+
+                    if (outbuf.Length > 0)
+                        File.WriteAllBytes(outputfile, outbuf);
+                }
+
+            }
+            else
+            {
+                ShowVersion();
+                Usage();
+            }
+        }
+
+        static int SearchArg(string[] args, string search)
+        {
+            for (int i = 0; i < args.Length; i++)
+                if (args[i] == search)
+                    return i;
+            return -1;
+        }
+
+        static void Usage()
+        {
+            Console.WriteLine("TransComp.exe -c/d [format] -i [input file] -o [output file] (-a <address in input/output file>) (-v)");
+            Console.WriteLine("    -c [format] - Compress Input file to Output file.");
+            Console.WriteLine("    -d [format] - Decompress Input file to Output file.");
+            Console.WriteLine("    -i [input file] - Set Input file.");
+            Console.WriteLine("    -o [output file] - Set Output file.");
+            Console.WriteLine("    -a [address] - Set address from Input / to Output file. (Optional, default = 0)");
+            Console.WriteLine("    -v - Verbose");
+        }
+
+        static void ShowVersion()
+        {
+            Console.WriteLine("TransComp v" + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString());
         }
     }
 }
